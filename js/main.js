0defae9fd84d19212a29f2ea94e6d59316b852c3
Tony Smith <tsmith.sounds@gmail.com>
2017-04-29 10:59:09 -0700

    Adjusted resolution. Won't fill sketch area.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 
 
 var $pixel = ("<div class='pixel'></div>");
+var r = 16;
+var newres = (r*r);
+
 
 $(document).ready(function() {
-	for (i=0; i<=255; i++) {
-  	$(".container").append($pixel);
-  }
+		for (i=0; i<=255; i++) {
+  		$(".container").append($pixel);
+	}
 });
 
 $(document).ready(function() {
@@ -14,20 +17,60 @@ $(document).ready(function() {
 	});	
 });
 
-/*
-$(document).ready(function() {
-	$(".clearbutton").on("click", function() {
-		$(".pixel").css("background-color", "white");
-	});
-});
-*/
 
 $(document).ready(function() {
-	$(".clearbutton").click(function() {
+	$("#clearbutton").click(function() {
 		$(".pixel").removeClass("pixel2");
 		$(".pixel").mouseenter(function() {
 			$(this).addClass("pixel2");
 		});
 	});
 });
+
+
+
+
+	$("#resizebutton").click(function() {
+		$(".pixel").removeClass("pixel2");
+		r = prompt("Pick a number!");
+			$(".pixel").css("height", (528/r));
+			$(".pixel").css("width", (528/r));
+		newres = (r*r);
+		resolution();
+	})	
+
+
+function resolution() {
+	for (i=0; i<=newres; i++) {
+  	$(".container").append($pixel);
+	}
+}
+		
+
+	
+
+		
+		
+		
+		/*
+		if (r<=100) {
+				$(".pixel").css("height", Math.floor(528/r));
+				$(".pixel").css("width", Math.floor(528/r));
+				for (i=0; i<=((r * r)-1); i++) {
+  					$(".container").append($pixel);
+				}
+			} else { 
+				(r=100)
+			}
+				$(".pixel").css("height", Math.floor(528/r));
+				$(".pixel").css("width", Math.floor(528/r));
+				for (i=0; i<=((r * r)-1); i++) {
+  					$(".container").append($pixel);
+				}
+		});
+		
+});
+
+*/
+
 
