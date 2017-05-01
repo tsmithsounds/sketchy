29cc9864f9078d6734494e0cd4f9e3553677aafd
Tony Smith <tsmith.sounds@gmail.com>
2017-04-30 19:35:46 -0700

    Finished first fully functional version.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
   
-  // Make Grid
-  
   var lengthwidth = 16;
+  makegrid(lengthwidth);
+  drawblack();
+  
+  $("#clearbutton").click(function() {
+    clear();
+  })
+  
+  $("#resizebutton").click(function() {
+    var newlengthwidth = prompt("Pick a number 1 to 100!");
+    clear();
+    makegrid(newlengthwidth);
+    drawblack();
+  })
+  
+  $("#drawconfetti").click(function() {
+    drawconfetti();
+  })
+  
+  $("#drawblack").click(function() {
+    drawblack(); 
+  })
+  
+  $("#eraser").click(function() {
+    erase();
+  })
+
+  // Grid
   
   function makegrid(lengthwidth) {
       for (i=0; i<=((lengthwidth * lengthwidth)-1); i++) {
@@ -11,37 +36,42 @@ $(document).ready(function() {
         $(".pixel").css("height", (700/lengthwidth));
         $(".pixel").css("width", (700/lengthwidth));
   }
-  
+    
   // Draw
   
-  function draw() {
+  function drawblack() {
     $(".pixel").mouseenter(function() {
-      $(this).addClass("pixel2");
+      $(this).css("background-color", "#333333");
+    })
+  }
+
+  // Draw Confetti
+
+  function drawconfetti() {
+    $(".pixel").mouseenter(function() {      
+      var random = 'rgb(' + (Math.floor((Math.random()*256)+1)) + ',' + (Math.floor((Math.random()*256)+1)) + ',' + (Math.floor((Math.random()*256)+1)) + ')';
+      console.log(random);
+      $(this).css("background-color", random);
+    })
+  }
+  
+  // Eraser
+  
+  function erase() {
+    $(".pixel").mouseenter(function() {
+      $(this).css("background-color", "white");
     })
   }
   
   // Clear
   
   function clear() {
-    $("#clearbutton").click(function() {
-      $(".pixel").removeClass("pixel2");
-    })
+      $(".pixel").css("background-color", "white");
   }
   
-makegrid(lengthwidth);
-
-draw();
-  
-clear();
-
-  // New Grid Resolution
-  
-    $("#resizebutton").click(function() {
-      $(".pixel").removeClass("pixel2");
-      newlengthwidth = prompt("Pick a number!");
-        makegrid(newlengthwidth);
-        draw();
-    })
 })
 
- 
+
+
+
+
